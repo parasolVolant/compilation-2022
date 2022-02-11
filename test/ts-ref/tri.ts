0005f90365589615dda100ec92470590e37dd0ae
Franck Dary <franck.dary@lis-lab.fr>
2022-02-11 13:53:12 +0100

    Added output references for SA and TS

---
+++ test/ts-ref/tri.ts
@@ -0,0 +1,21 @@
+TABLE GLOBALE
+tab	VAR  	10	0
+afficher	FCT	1
+echanger	FCT	2
+initialiser	FCT	0
+main	FCT	0
+trier	FCT	1
+TABLE LOCALE : afficher
+i	VAR  	1	0
+n	PARAM	1	0
+TABLE LOCALE : echanger
+i	PARAM	1	0
+j	PARAM	1	1
+temp	VAR  	1	0
+TABLE LOCALE : initialiser
+TABLE LOCALE : main
+TABLE LOCALE : trier
+echange	VAR  	1	0
+j	VAR  	1	1
+m	VAR  	1	2
+n	PARAM	1	0
