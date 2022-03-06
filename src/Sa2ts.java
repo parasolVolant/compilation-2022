cd6de9f53b6d83e6d690412f20ec6870e14f5f11
d16011503 <miryam.djebbari@etu.univ-amu.fr>
2022-03-06 18:06:04 +0100

    avancement Sa2ts

--- src/Sa2ts.java
+++ src/Sa2ts.java
@@ -18,46 +18,54 @@ public class Sa2ts extends SaDepthFirstVisitor {
     public Object visit(SaDecTab node) {
         String identif = node.getNom();
         int size = node.getTaille();
-        tableGlobale.addVar(identif,size);
-        TsItemVar itemVar = new TsItemVar(identif, size);
-        return itemVar;
-    }
-
-    private void addToLocalTable(SaLDec list){
-        SaDecVar var = (SaDecVar) list.getTete();
-        String identif = var.getNom();
-        tableLocaleCourante.addVar(identif, 1);
-        list = list.getQueue();
-        while(list.getTete() != null) {
-            addToLocalTable(list);
-        }
+        return tableGlobale.addVar(identif,size);
     }
 
     @Override
     public Object visit(SaDecFonc node) {
-        String adrArgCourantidentif = node.getNom();
+        Ts tableLocale = new Ts();
+        String identif = node.getNom();
         int nbArgs = node.getParametres().length();
         SaLDec list = node.getVariable();
-        return super.visit(node);
+        for (int i = 0; i < list.length() ; i++) {
+            tableLocale.addParam(list.getTete().getNom());
+        }
+        tableLocaleCourante = tableLocale;
+        return tableGlobale.addFct(identif,nbArgs,tableLocale,node);
     }
 
     @Override
     public Object visit(SaDecVar node) {
-        return super.visit(node);
+        String identif = node.getNom();
+        int size = 1;
+        return tableGlobale.addVar(identif,size);
     }
 
     @Override
     public Object visit(SaVarSimple node) {
-        return super.visit(node);
+        String identif = node.getNom();
+        int size = 1;
+        if (tableLocaleCourante.getVar(identif) != null) return tableLocaleCourante.getVar(identif);
+        else if (tableLocaleCourante.getVar(identif).isParam) return tableLocaleCourante.getVar(identif);
+        else if (tableGlobale.getVar(identif) == null) return tableGlobale.getVar(identif);
+        return null;
     }
 
     @Override
     public Object visit(SaAppel node) {
-        return super.visit(node);
+        String identif = node.getNom();
+        SaLExp list = node.getArguments();
+        int nbArgs = list.length();
+        if (tableGlobale.getFct(identif) == null) return null;
+        if (tableGlobale.getFct(identif).getNbArgs() != nbArgs) return null;
+        if (tableGlobale.getFct("main") == null || tableGlobale.getFct("main").getNbArgs() != 0) return null;
+        return tableGlobale.
     }
 
     @Override
     public Object visit(SaVarIndicee node) {
-        return super.visit(node);
+        String identif = node.getNom();
+        int indice = Integer.parseInt(node.getIndice().toString());
+
     }
 }
