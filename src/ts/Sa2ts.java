e7d492f248b15e0ddb400f7aa0aad9ccf47212d7
d16011503 <miryam.djebbari@etu.univ-amu.fr>
2022-03-08 22:00:42 +0100

    sa2ts finie problèmes à régler

--- src/ts/Sa2ts.java
+++ src/ts/Sa2ts.java
@@ -1,10 +1,13 @@
 package ts;
 
 import sa.*;
-import ts.Ts;
-import ts.TsItemVar;
 
-public class Sa2ts extends SaDepthFirstVisitor<Object> {
+public class Sa2ts extends SaDepthFirstVisitor<Void> {
+
+    public Sa2ts(SaProg prog) {
+        super();
+
+    }
 
     enum Context {LOCAL, GLOBAL, PARAM}
 
@@ -17,61 +20,169 @@ public class Sa2ts extends SaDepthFirstVisitor<Object> {
 
 
     @Override
-    public Object visit(SaDecTab node) {
-        String identif = node.getNom();
-        int size = node.getTaille();
-        if (tableGlobale.getVar(identif) != null) return null;
-        return tableGlobale.addVar(identif,size);
-    }
+    public Void visit(SaDecTab node) {
 
-    @Override
-    public Object visit(SaDecFonc node) {
-        Ts tableLocale = new Ts();
+        defaultIn(node);
         String identif = node.getNom();
-        int nbArgs = node.getParametres().length();
-        SaLDec list = node.getVariable();
-        for (int i = 0; i < list.length() ; i++) {
-            if (tableLocale.getVar(identif) != null) return null;
-            tableLocale.addParam(list.getTete().getNom());
+        int taille = node.getTaille();
+        node.tsItem = tableGlobale.variables.get(identif);
+        if (node.tsItem == null) {
+            context = Context.GLOBAL;
+            node.tsItem = tableGlobale.addVar(identif,taille);
+
         }
-        tableLocaleCourante = tableLocale;
-        return tableGlobale.addFct(identif,nbArgs,tableLocale,node);
+        else  new Exception();
+        defaultOut(node);
+        return null;
+
+
+
     }
 
     @Override
-    public Object visit(SaDecVar node) {
-        String identif = node.getNom();
-        int size = 1;
-        if (tableGlobale.getVar(identif) != null) return null;
-        return tableGlobale.addVar(identif,size);
-    }
+    public Void visit(SaDecFonc node) {
+        defaultIn(node);
 
-    @Override
-    public Object visit(SaVarSimple node) {
         String identif = node.getNom();
-        int size = 1;
-        if (tableLocaleCourante.getVar(identif) != null) return tableLocaleCourante.getVar(identif);
-        else if (tableLocaleCourante.getVar(identif).isParam) return tableLocaleCourante.getVar(identif);
-        else if (tableGlobale.getVar(identif) == null) return tableGlobale.getVar(identif);
+        int nbArgs;
+        node.tsItem = tableGlobale.fonctions.get(identif);
+
+        if(node.tsItem == null){
+
+           if (node.getParametres() == null) nbArgs = 0;
+
+           else nbArgs = node.getParametres().length();
+
+           tableLocaleCourante = new Ts();
+
+           node.tsItem = tableGlobale.addFct(identif,nbArgs,tableLocaleCourante,node);
+
+            context = Context.PARAM;
+
+            if(node.getParametres() != null) this.visit(node.getParametres());
+
+            context = Context.LOCAL;
+
+            if(node.getVariable() != null) this.visit(node.getVariable());
+
+            if(node.getCorps() != null) node.getCorps().accept(this);
+
+            context = Context.GLOBAL;
+    }
+        else  new Exception();
+
+        defaultOut(node);
+
         return null;
     }
 
     @Override
-    public Object visit(SaAppel node) {
+    public Void visit(SaDecVar node) {
+        defaultIn(node);
+
         String identif = node.getNom();
-        SaLExp list = node.getArguments();
-        int nbArgs = list.length();
-        if (tableGlobale.getFct(identif) == null) return null;
-        if (tableGlobale.getFct(identif).getNbArgs() != nbArgs) return null;
-        if (tableGlobale.getFct("main") == null || tableGlobale.getFct("main").getNbArgs() != 0) return null;
-        return tableGlobale.getVar(identif);
+        int taille = 1;
+        node.tsItem = tableGlobale.variables.get(identif);
+        if (node.tsItem == null) {
+
+        tableGlobale.addVar(identif,taille);
+        context = Context.GLOBAL;
+
+
+        }
+
+
+        else  new Exception();
+
+        defaultOut(node);
+
+        return null;
     }
 
     @Override
-    public Object visit(SaVarIndicee node) {
+    public Void visit(SaVarSimple node) {
+        defaultIn(node);
+        String identif = node.getNom();
+        int taille = 1;
+
+        context = Context.LOCAL;
+        node.tsItem = tableLocaleCourante.getVar(identif);
+        if (node.tsItem != null)  new Exception();
+
+        context = Context.PARAM;
+        node.tsItem = tableLocaleCourante.getVar(identif);
+        if (node.tsItem.isParam)  try {
+            throw new Exception();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        context = Context.GLOBAL;
+        node.tsItem = tableGlobale.getVar(identif);
+        if (node.tsItem == null)  try {
+            throw new Exception();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        defaultOut(node);
+        return null;
+    }
+
+    @Override
+    public Void visit(SaAppel node) {
+        defaultIn(node);
+        String identif = node.getNom();
+        int nbArgs;
+
+        node.tsItem = tableGlobale.fonctions.get(identif);
+
+        if(node.tsItem != null) {
+
+            if (node.getArguments() == null) nbArgs = 0;
+
+            else nbArgs = node.getArguments().length();
+
+
+            context = Context.PARAM;
+            if (node.tsItem.getNbArgs() != nbArgs) new Exception();
+
+            if (node.getArguments() != null) this.visit(node.getArguments());
+
+            if (tableGlobale.fonctions.get("main") == null || tableGlobale.fonctions.get("main").getNbArgs() != 0)
+                new Exception();
+
+        }
+
+        else new Exception();
+
+        defaultOut(node);
+        return null;
+
+    }
+
+    @Override
+    public Void visit(SaVarIndicee node) {
+
+        defaultIn(node);
+
         String identif = node.getNom();
         int indice = Integer.parseInt(node.getIndice().toString());
-        if (indice == null) return null;
 
+        node.tsItem = tableLocaleCourante.variables.get(identif);
+
+        if (node.tsItem != null){
+        context = Context.GLOBAL;
+        this.visit(node.getIndice());
+        }
+        defaultOut(node);
+
+
+
+        return null;
+    }
+
+    public Ts getTableGlobale() {
+        return tableGlobale;
     }
 }
